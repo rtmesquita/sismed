254e0119a588e9807a94d524a834e522d49e0922
Rafael Teles Mesquita <rafaeltelesm@gmail.com>
2024-06-16 14:34:23 -0300

    primeiro commit - Projeto estruturado

---
+++ src/main/java/com/sismed/sismed/SismedApplication.java
@@ -0,0 +1,13 @@
+package com.sismed.sismed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SismedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SismedApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/sismed/sismed/controller/HomeController.java
@@ -0,0 +1,12 @@
+package com.sismed.sismed.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+    @RequestMapping("/")
+    public String index () {
+        return "index";
+    }
+}
